98df3d8bfafafa92fca2f8745eff66c4d58f2d51
Lavith-E <lavith.edam@priviahealth.com>
2024-09-02 12:27:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lvk_todo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lvk_todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lvk_todo.ui.theme.LVK_TODOTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Text(text = "Ashuuuu")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun showText(name : String = "Laduuuu"){
+    Text(text = name)
+}
